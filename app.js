7b03a992c0ee238b08eadf3e3fcea41af62e443f
36rustam <muhammadievspaces@gmail.com>
2024-12-18 16:02:01 +0200

    inicio2

---
+++ app.js
@@ -0,0 +1,13 @@
+// dotend (.env)
+require ('dotenv').config();
+
+const express = require ('express');
+const app = express();
+const port= process.env.PORT || 8080;
+
+app.get('/', (req,res)=>{
+    res.send('inicio');
+})
+app.listen(port,()=>{
+    console.log(`http://localhost:${port}`);
+})
