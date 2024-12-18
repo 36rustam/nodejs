0bb025828fca2cf56ae32b943666cebe73048780
36rustam <muhammadievspaces@gmail.com>
2024-12-18 16:22:20 +0200

    inicio5

--- app.js
+++ app.js
@@ -8,6 +8,13 @@ const port= process.env.PORT || 3000;
 app.get('/', (req,res)=>{
     res.sendFile(`${__dirname}/public/index.html`);
 })
+app.get('/content1',(req, res)=>{
+    res.sendFile(`${__dirname}/public/content1.html`);
+})
+app.get('*', (req,res)=>{
+    res.sendFile(`${__dirname}/404.html`)
+})
+
 app.listen(port,()=>{
     console.log(`http://localhost:${port}`);
 })
